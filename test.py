50ce84edb79281eacfaad0ff04926888b2b6a973
RohanShetty <shettyrohan641@yahoo.com>
2020-07-14 22:55:06 +0530

    Depth Maps

--- test.py
+++ test.py
@@ -2,12 +2,13 @@ import os
 import glob
 import argparse
 import matplotlib
+import numpy as np
 
 # Keras / TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 from keras.models import load_model
 from layers import BilinearUpSampling2D
-from utils import predict, load_images, display_images
+from utils import predict_, load_images, display_images
 from matplotlib import pyplot as plt
 
 # Argument Parser
@@ -26,19 +27,13 @@ model = load_model(args.model, custom_objects=custom_objects, compile=False)
 
 print('\nModel loaded ({0}).'.format(args.model))
 
-# Input images
-inputs = load_images( glob.glob(args.input) )
-print('\nLoaded ({0}) images of size {1}.'.format(inputs.shape[0], inputs.shape[1:]))
 
-# Compute results
-outputs = predict(model, inputs)
-
-#matplotlib problem on ubuntu terminal fix
-#matplotlib.use('TkAgg')   
-
-# Display results
-viz = display_images(outputs.copy(), inputs.copy())
-plt.figure(figsize=(10,5))
-plt.imshow(viz)
-plt.savefig('test.png')
-plt.show()
+def depth_images(image_files, batch_size):
+    """
+    Func: Returns Depth Maps
+    """
+    dpmap = np.empty([len(image_files), 224,224,1], dtype=np.uint8) # returns an empty of given shape without any entries
+    for i in range(len(image_files)//batch_size):
+        inputs = load_images(image_files)
+        dpmap[i*batch_size:(i+1)*batch_size,...] = predict_(model, inputs)
+        return dpmap

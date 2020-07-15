0f4add27f022bf0bc16d601e0e1a9ce616f3d181
Rohan Shetty <40785407+Gilf641@users.noreply.github.com>
2020-07-15 16:05:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,19 @@ import os
 import glob
 import argparse
 import matplotlib
-import numpy as np
 
 # Keras / TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 from keras.models import load_model
 from layers import BilinearUpSampling2D
-# from utils import predict_, load_images, display_images
+from utils import predict, load_images, display_images
 from matplotlib import pyplot as plt
 
+# Argument Parser
+parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
+parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
+parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
+args = parser.parse_args()
 
 # Custom object needed for inference and training
 custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
@@ -18,17 +22,18 @@ custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_func
 print('Loading model...')
 
 # Load model into GPU / CPU
-model_path = "./nyu.h5"
-model = load_model(model_path, custom_objects=custom_objects, compile=False)
+model = load_model(args.model, custom_objects=custom_objects, compile=False)
 
-print('\nModel loaded ({0}).'.format(model_path))
+print('\nModel loaded ({0}).'.format(args.model))
 
-def depth_images(image_files, batch_size):
-    """
-    Func: Returns Depth Maps
-    """
-    dpmap = np.empty([len(image_files), 224,224,1], dtype=np.float) # returns an empty of given shape without any entries
-    for i in range(len(image_files)//batch_size):
-        inputs = load_images(image_files)
-        dpmap[i*batch_size:(i+1)*batch_size,...] = predict_(model, inputs)
-        return dpmap
+# Input images
+inputs = load_images( glob.glob(args.input) )
+print('\nLoaded ({0}) images of size {1}.'.format(inputs.shape[0], inputs.shape[1:]))
+
+# Compute results
+outputs = predict(model, inputs)
+
+
+#matplotlib problem on ubuntu terminal fix
+#matplotlib.use('TkAgg')   
+
